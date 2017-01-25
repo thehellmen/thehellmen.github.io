933737a9b99b68b2b22087c94e8f694ad533657d
thehellmen <buildy57@gmail.com>
2017-01-25 14:03:49 -0500

    Create main.js

---
+++ data/main.js
@@ -0,0 +1,12 @@
+var sketchProc=function(processingInstance){ with (processingInstance){
+
+setup = function() {
+	size(1000,800);
+	frameRate(60);
+};
+
+draw = function() {
+	background(255,255,255);
+};
+
+}};
