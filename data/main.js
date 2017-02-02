584d48c0d3a8dc916b13239a0ef6da19864c09ba
thehellmen <buildy57@gmail.com>
2017-02-02 12:45:44 -0500

    Update main.js

--- data/main.js
+++ data/main.js
@@ -208,7 +208,7 @@ setup = function() {
 		pushMatrix();
 		translate(width/2,height/2);
 		scale(0.001*height,0.001*height);
-			tint(200,200,200,flevel*5);
+			tint(140,140,140,flevel*2);
 			image(smoke,0,0);
 			noTint();
 		popMatrix();		
